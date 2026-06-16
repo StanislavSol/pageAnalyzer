8922bb5fa5bf806fd1e18a9b60a94bae72fce735
Solovtsov Stanislav <95217662+StanislavSol@users.noreply.github.com>
2026-06-16 23:56:53 +0300

    Update HtmlCheck.php

--- src/HtmlCheck.php
+++ src/HtmlCheck.php
@@ -4,8 +4,6 @@ namespace App;
 
 use DiDom\Document;
 
-const FIRST_INDEX_ELEMENT = 0;
-
 class HtmlCheck
 { 
     private $url;
@@ -24,28 +22,35 @@ class HtmlCheck
             "title" => null,
             "description" => null
         ];
+        
         try {
-            $request = $client->request('GET',  $this->url);
+            $request = $client->request('GET', $this->url);
             $document = new Document($this->url, true);
 
             $resultCheck["statusCode"] = $request->getStatusCode();
-            $resultCheck["h1"] = $document->find("h1")[FIRST_INDEX_ELEMENT]?->text();
-            $resultCheck["title"] = $document->find("title")[FIRST_INDEX_ELEMENT]?->text();
+            
+            $h1Elements = $document->find("h1");
+            $resultCheck["h1"] = isset($h1Elements[0]) ? $h1Elements[0]->text() : null;
+            
+            $titleElements = $document->find("title");
+            $resultCheck["title"] = isset($titleElements[0]) ? $titleElements[0]->text() : null;
 
-            if ($document->has('meta[name=description]')) {
-                $resultCheck["description"] = $document->find('meta[name=description]')[FIRST_INDEX_ELEMENT]->content;
+            $descElements = $document->find('meta[name=description]');
+            if (isset($descElements[0])) {
+                $resultCheck["description"] = $descElements[0]->content;
             }
+            
             $resultCheck["message"] = ["success", "Страница успешно проверена"];
 
-	} catch (\GuzzleHttp\Exception\ClientException $e) { 
+        } catch (\GuzzleHttp\Exception\ClientException $e) { 
             $resultCheck["message"] = ["warning", "Проверка была выполнена успешно, но клиент ответил с ошибкой"];
             $resultCheck["statusCode"] = 403;
         } catch (\GuzzleHttp\Exception\ConnectException $e) {
             $resultCheck["message"] = ["danger", "Произошла ошибка при проверке, не удалось подключиться"];
-	}
+        } catch (\Exception $e) {
+            $resultCheck["message"] = ["danger", "Произошла ошибка: " . $e->getMessage()];
+        }
         
         return $resultCheck;
-
     }
-
 }
